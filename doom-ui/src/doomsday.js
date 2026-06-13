83a989d07637999ff914a6049305996b951961f0
Utsab Mondal <utsabmondal19@gmail.com>
2026-06-13 16:34:21 -0400

    Added doomsday calculator function

--- doom-ui/src/doomsday.js
+++ doom-ui/src/doomsday.js
@@ -2,11 +2,30 @@
 const anchor = '2053';
 
 export const doomsdayCalculator = (month, day, year) => {
+    // dealing with bc dates
     if (year < 0) {
-        year--;
-        year = -year;
+        year++;
         year %= 400;
         year += 400;
     }
     
+    // y = last 2 digits
+    let y = year % 100;
+
+    let y$ = y % 2 != 0 ? y + 11 : y;
+    y$ /= 2;
+
+    let y$$ = y$ % 2 != 0 ? y$ + 11 : y$;
+
+    let s = -y$$ % 7;
+
+    // c = everything except last 2 digits
+    let c = year / 100;
+
+    let a = Number(anchor[c % 4 | 0]); // drops the decimals
+
+    let d = (a + s) % 7;
+    console.log(d);
 };
+
+doomsdayCalculator(1, 2, -7);
